9e7852ec68731e9985f0a001a9180604ce1bf286
Ulvy156 <ulvyromy156@gmail.com>
2024-12-22 22:17:31 +0700

    display details for each course

---
+++ src/components/TestYoi.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            coursesDetails: [
+                {
+                    id: "c",
+                    bgColor: "bg-blue-200",
+                    programming_lng: "C",
+                    chapters: 10,
+                    lessons: 3,
+                    exams: 4,
+                    files: 3
+                }
+            ],
+        }
+    }
+}
+</script>
